797bbde87a49f6226fd6bfb8db5ffa2ddc141218
cod500 <cod5000@gmail.com>
2021-10-01 22:37:32 -0400

    process env config

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@ import * as admin from 'firebase-admin';
 import credentials from './credentials.json';
 import { db } from './db';
 import { routes } from './routes';
+import dotenv from 'dotenv';
 
 admin.initializeApp({ credential: admin.credential.cert(credentials) });
 
 const app = express();
+dotenv.config();
+
 
 app.use(bodyParser.json());
 
@@ -15,10 +18,12 @@ routes.forEach(route => {
     app[route.method](route.path, route.handler);
 });
 
+const PORT = process.env.PORT || 8080;
+
 const start = async () => {
-    await db.connect('mongodb://localhost:27017');
-    app.listen(8080, () => {
-        console.log('Server is listening on port 8080');
+    await db.connect(process.env.CONNECTION_URL);
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
     });
 }
 
